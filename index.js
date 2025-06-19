cf484fc61b8c2950ccc20a138efa5ad25f1eed7e
Brian-Automation-Guru <musabrianno5@gmail.com>
2025-06-19 18:04:46 +0000

    First commit intergrating with HubSpot I: Foundations practicum repository.

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+require('dotenv').config();
+
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PORT = process.env.PORT || 3000;
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+const accessToken = process.env.TOKEN;
 
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
+app.get('/', async (req, res) => {
+    const AirDefSystems = 'https://api.hubspot.com/crm/v3/objects/';
     const headers = {
-        Authorization: `Bearer ${process.env.PRIVATE_APP_ACCESS}`,
+        Authorization: `Bearer ${accessToken}`,
         'Content-Type': 'application/json'
     }
     try {
